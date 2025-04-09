57a919cd16eef42e51f0ef3338f2596420ffa26f
foxkon24 <foxkon24@gmail.com>
2025-04-09 11:29:54 +0900

    Add files via upload

--- main/start_cam.bat
+++ main/start_cam.bat
@@ -2,8 +2,23 @@
 
 @if not "%~0"=="%~dp0.\%~nx0" start /min cmd /c,"%~dp0.\%~nx0" %* & goto :eof
 
-echo Starting camera monitoring system with elevated privileges...
+cd D:\laragon\www\system\cam
 
-powershell -Command "Start-Process cmd -ArgumentList '/c cd %~dp0 && python app.py' -Verb RunAs"
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
 
-pause
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+    "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+    cd /d "%~dp0"
+    echo Running with administrative privileges.
+    python app.py
+    pause
